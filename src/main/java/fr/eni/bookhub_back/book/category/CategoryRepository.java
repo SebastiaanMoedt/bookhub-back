7faf7bb695366a50bc2e5caac662dd9074870d89
eveningdreamer <eveningdreamer@proton.me>
2026-04-21 13:56:56 +0200

    repos2

---
+++ src/main/java/fr/eni/bookhub_back/book/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package fr.eni.bookhub_back.book.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
+}
