f84d95f1945fb1ac7fd4f8cba5a5bea7cc4f544c
Mickaël Mesloub <mickael.mesloub@gmail.com>
2026-04-22 15:25:41 +0200

    fix missing import

--- src/main/java/fr/eni/bookhub_back/book/BookService.java
+++ src/main/java/fr/eni/bookhub_back/book/BookService.java
@@ -2,14 +2,16 @@ package fr.eni.bookhub_back.book;
 
 import fr.eni.bookhub_back.common.ServiceResponse;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @AllArgsConstructor
