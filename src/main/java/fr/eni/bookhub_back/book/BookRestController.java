6683b25b3b5d8f54a640b4206bc8b3ee119c9694
eveningdreamer <eveningdreamer@proton.me>
2026-04-22 10:28:33 +0200

    catalog is now books

--- src/main/java/fr/eni/bookhub_back/book/BookRestController.java
+++ src/main/java/fr/eni/bookhub_back/book/BookRestController.java
@@ -4,6 +4,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,15 +13,23 @@ import java.util.Optional;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/catalog")
+@RequestMapping("/api/books")
 public class BookRestController {
 
         @Autowired
         private BookRepository bookRepository;
 
-        @GetMapping("/{page}")
-        public ResponseEntity<?> allBooks(@PathVariable Integer page) {
+        @GetMapping
+        public ResponseEntity<?> allBooksPage0(){
+            int size = 20;
+            Pageable pageable = PageRequest.of(0, size);
+            Page<Book> pageBook = bookRepository.findAll(pageable);
 
+            return ResponseEntity.ok(pageBook.getContent());
+        }
+
+        @GetMapping("/{page}")
+        public ResponseEntity<?> allBooks(@PathVariable(required = false) Integer page) {
             int size = 20;
             Pageable pageable = PageRequest.of(page, size);
             Page<Book> pageBook = bookRepository.findAll(pageable);
