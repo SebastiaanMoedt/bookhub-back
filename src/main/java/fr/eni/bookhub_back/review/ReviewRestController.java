6b7307cbf62ec3d7db0a1c26f76a9fa3c4c0994a
eveningdreamer <eveningdreamer@proton.me>
2026-04-27 15:34:18 +0200

    review working

--- src/main/java/fr/eni/bookhub_back/review/ReviewRestController.java
+++ src/main/java/fr/eni/bookhub_back/review/ReviewRestController.java
@@ -1,7 +1,11 @@
 package fr.eni.bookhub_back.review;
 import fr.eni.bookhub_back.book.BookRepository;
 import fr.eni.bookhub_back.book.BookService;
+import fr.eni.bookhub_back.common.ServiceResponse;
+import fr.eni.bookhub_back.loan.Loan;
+import fr.eni.bookhub_back.loan.LoanDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,5 +24,15 @@ public class ReviewRestController {
                                            @RequestParam(name = "isbn") String isbn) {
 
         return reviewService.findAllByBook(page, size, sort, isbn);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ServiceResponse<Review>> createReview(@RequestBody ReviewDTO reviewDTO) {
+        try {
+            return reviewService.saveReview(reviewDTO);
+        } catch (RuntimeException e){
+            ServiceResponse<Review> response = new ServiceResponse<>("REVIEW_SAVE_FAILED", "{review.save-failed-error}");
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
+        }
     }
 }
--- src/main/java/fr/eni/bookhub_back/review/ReviewService.java
+++ src/main/java/fr/eni/bookhub_back/review/ReviewService.java
@@ -3,7 +3,15 @@ package fr.eni.bookhub_back.review;
 import fr.eni.bookhub_back.book.Book;
 import fr.eni.bookhub_back.book.BookRepository;
 import fr.eni.bookhub_back.book.BookService;
+import fr.eni.bookhub_back.book.bookcopy.BookCopy;
 import fr.eni.bookhub_back.common.ServiceResponse;
+import fr.eni.bookhub_back.loan.Loan;
+import fr.eni.bookhub_back.loan.LoanService;
+import fr.eni.bookhub_back.locale.LocaleHelper;
+import fr.eni.bookhub_back.user.bo.User;
+import fr.eni.bookhub_back.user.dao.jpa.UserJpaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,24 +21,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewService {
     @Autowired
     ReviewRepository reviewRepository;
-
     @Autowired
     BookService bookService;
+    @Autowired
+    BookRepository bookRepository;
+    @Autowired
+    private LocaleHelper localeHelper;
+    @Autowired
+    private UserJpaRepository userRepository;
+
+    private final static Logger logger = LoggerFactory.getLogger(ReviewService.class);
 
     public ResponseEntity<ServiceResponse<Page<Review>>> findAllByBook(int page, int size, String sortBy, String isbn) {
         try {
             Book book = bookService.findBookObjectByISBN(isbn).get();
-            Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+            Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy).descending());
             Page<Review> pageReview = reviewRepository.findAllByBook(pageable, book);
-            if(pageReview.isEmpty()){
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ServiceResponse<>("REVIEWS_NOT_FOUND",null));
-            }
             ServiceResponse<Page<Review>> response =
                     new ServiceResponse<>("REVIEWS_FOUND", "Commentaires trouvés", pageReview);
             return ResponseEntity.ok(response);
@@ -38,6 +52,47 @@ public class ReviewService {
             ServiceResponse<Page<Review>> response =
                     new ServiceResponse<>("REVIEWS_NOT_FOUND", e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+    }
+
+    public ResponseEntity<ServiceResponse<Review>> saveReview(ReviewDTO reviewDTO){
+
+        if (reviewDTO.bookISBN == null || reviewDTO.username == null) {
+            ServiceResponse<Review> response = new ServiceResponse<>("BOOK_NULL", localeHelper.i18n("book.null-error"));
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
+        }
+
+        Optional<User> optUser = userRepository.findByUsername(reviewDTO.username);
+        if (optUser.isEmpty()) {
+            ServiceResponse<Review> response = new ServiceResponse<>("USER_DOES_NOT_EXIST", localeHelper.i18n("user.not-found"));
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
+        }
+
+        Optional<Book> optBook = bookRepository.findBookByIsbn(reviewDTO.bookISBN);
+
+        if (optBook.isEmpty()) {
+            ServiceResponse<Review> response = new ServiceResponse<>("BOOK_DOES_NOT_EXIST", localeHelper.i18n("book.isbn-doesnt-exist-error"));
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
+        }
+
+        if(reviewDTO.comment == null){
+            ServiceResponse<Review> response = new ServiceResponse<>("REVIEW_IS_EMPTY", localeHelper.i18n("review.comment-empty-error"));
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
+        }
+
+        try {
+            Review review = Review.builder()
+                    .datePublished(LocalDate.now())
+                    .book(optBook.get())
+                    .user(optUser.get())
+                    .score(reviewDTO.score).comment(reviewDTO.comment).build();
+            reviewRepository.save(review);
+            ServiceResponse<Review> response = new ServiceResponse<>("REVIEW_SAVED", localeHelper.i18n("review.saved-success"), review);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage());
+            ServiceResponse<Review> response = new ServiceResponse<>("REVIEW_SAVE_FAILED", localeHelper.i18n("review.save-failed-error"));
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
         }
     }
 }
